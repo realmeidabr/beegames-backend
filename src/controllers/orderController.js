73d01085195283c70529a5af5fe04921a8393305
realmeidabr <realmeida.br@gmail.com>
2022-07-21 07:40:54 -0300

    initial commit

---
+++ src/controllers/orderController.js
@@ -0,0 +1,26 @@
+import IOClient from '../services/index.js'
+import { calcPoints } from '../functions/calc.js'
+
+class OrderController {
+
+  getFeedPing = async (req, res) => {
+    const feedData = req.body
+    const orderId = feedData.OrderId
+    this.dispatchFlowUpdates(orderId)
+    res.send({ success: true })
+  }
+
+  dispatchFlowUpdates = async (orderId) => {
+    const orderData = await IOClient.getOrderData(orderId)
+    const clientId = orderData.clientProfileData.userProfileId
+    const orderValue = orderData.totals[0].value
+    const balance = await IOClient.getUserPointsBalance(clientId)
+    const newBalance = calcPoints(balance, orderValue, 'sum')
+    await IOClient.updateUserPointsBalance(clientId, newBalance)
+  }
+}
+
+export default new OrderController
+
+
+// Tested via API client only (FeedV3 Order Hook dependent)
